f59222651b08a4badf747bc43134753785569638
Newton96-cloud <odeinewton@gmail.com>
2023-11-16 23:48:50 -0800

    fixed bud

--- _atoi.c
+++ _atoi.c
@@ -6,6 +6,8 @@
 int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+interactive(info_t *info): The function checks if the shell is in interactive mode by using the isatty() function to determine if the standard input is a terminal and checking the file descriptor readfd in the info structure to be less than or equal to 2 (which typically refers to standard input, output, and error). It returns 1 if it's in interactive mode and 0 otherwise.
+
 }
 
 /**
@@ -18,7 +20,9 @@ int is_delim(char c, char *delim)
 		if (*delim++ == c)
 			return (1);
 	return (0);
+is_delim(char c, char *delim): The function checks if a given character c is a delimiter by iterating through the characters in the delim string and comparing them to c. If a match is found, it returns 1; otherwise, it returns 0.
 }
+
 
 /**
  * _isalpha - checks for alphabetic character
@@ -26,6 +30,10 @@ int is_delim(char c, char *delim)
 
 int _isalpha(int c)
 {
+_isalpha(int c): The function checks  a given character c is an alphabetic character (either uppercase or lowercase). It uses ASCII values to determine if c falls within the range of alphabetic characters ('a' to 'z' or 'A' to 'Z') and returns 1 if it's an alphabetic character and 0 otherwise.
+
+
+
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
